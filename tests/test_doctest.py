ecdbb031f239828f10cb39ac2a6602dffc87bab5
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2017-07-06 18:58:05 +0100

    maintenance: try to doctest everything

--- tests/test_doctest.py
+++ tests/test_doctest.py
@@ -1,4 +1,4 @@
-import doctest
+from doctest import testmod
 import unittest
 
 import testdata
@@ -6,13 +6,15 @@ import testdata
 class TestDocTest(unittest.TestCase):
 
     def test_doctest(self):
-        doctest.testmod(testdata.base)
-        doctest.testmod(testdata.dictionary)
-        doctest.testmod(testdata.factories.statistical)
-        doctest.testmod(testdata.factories.datetimes)
-        doctest.testmod(testdata.factories.generic)
-        doctest.testmod(testdata.factories.sequences)
-        doctest.testmod(testdata.factories.numbers)
+        testmod(testdata.base)
+        testmod(testdata.dictionary)
+        testmod(testdata.factories.datetimes)
+        testmod(testdata.factories.fake)
+        testmod(testdata.factories.generic)
+        testmod(testdata.factories.numeric)
+        testmod(testdata.factories.sequences)
+        testmod(testdata.factories.statistical)
+        testmod(testdata.factories.strings)
 
 if __name__ == '__main__':
     unittest.main()
