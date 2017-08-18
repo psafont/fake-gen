5c3c534fca73169257c342437083f46fa637e9c1
Pau Ruiz i Safont <psafont@ebi.ac.uk>
2017-08-18 16:48:37 +0100

    maintenance: tweak setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
-import os
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
-def fread(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def get_contents(filename):
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, filename), encoding='utf-8') as f:
+        contents = f.read()
+    return contents
 
-def get_version():
-    VERSION = fread("VERSION").strip()
-    build_number = os.environ.get('BUILD_NUMBER', None)
-    if build_number:
-        return VERSION + "b{}".format(build_number)
-    return VERSION
 
 setup(
-    name = "python-testdata",
-    version = get_version(),
+    name = "python-testdata-tsi",
+    # https://pypi.python.org/pypi/setuptools_scm
+    use_scm_version = True,
+
     author = "Arie Bro",
     author_email = "ariebro@gmail.com",
+    maintainer = "Pau Ruiz Safont",
+    maintainer_email = "psafont@ebi.ac.uk",
     description = "A small package that helps generate content to fill databases for tests.",
-    url = "http://github.com/arieb/python-testdata",
+    url = "http://github.com/psafont/python-testdata",
     license = "MIT",
-    long_description=fread('README.md'),
-    classifiers=[
+    long_description = get_contents('README.md'),
+    classifiers = [
         'Development Status :: 3 - Alpha',
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Testing",
@@ -33,7 +35,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -41,13 +42,16 @@ setup(
     keywords = "factory testing test unittest mongo data testdata database json elasticsearch",
 
     packages = find_packages(exclude=['docs', 'tests']),
-    include_package_data=True,
+    include_package_data = True,
 
     install_requires = [
         'fake-factory == 0.3.2'
     ],
-        extras_require={
-            'test': ['pytest', 'pymongo'],
-            'mongo': ['pymongo']
-        },
+    setup_requires = [
+        'setuptools_scm'
+    ],
+    extras_require = {
+        'test': ['pytest', 'pymongo'],
+        'mongo': ['pymongo']
+    },
 )
